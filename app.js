d5d3b3255f03eb294fdd4f51e6200e7604c1e416
Thomas Le Magoariec <thomas.lemagoariec@outlook.com>
2025-01-14 08:42:21 +0100

    added alert

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const discover = document.querySelector(".discover");
+const download = document.querySelector(".download")
 
 discover.addEventListener("click", () => {
     window.location.href = "/discover"
+})
+
+download.addEventListener("click", () => {
+    alert("The game isn't available right now, but be sure to come back later to check it out!");
 })
--- discover/app.js
+++ discover/app.js
@@ -1,5 +1,10 @@
 const back = document.querySelector(".back");
+const download = document.querySelector(".download");
 
 back.addEventListener("click", () => {
     window.location.href = "/";
+})
+
+download.addEventListener("click", () => {
+    alert("The game isn't available right now, but be sure to come back later to check it out!");
 })
